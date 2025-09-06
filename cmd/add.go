e8f6d78cc1e7f19abcdd2e94f0f3e8d94ecb70e0
prnvbn <prnvbn@gmail.com>
2025-09-06 21:53:43 +0100

    Update add cmd to use refactored run methods

--- cmd/add.go
+++ cmd/add.go
@@ -1,16 +1,13 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
-	"iter"
-	"os"
 
 	"github.com/prnvbn/bq/internal/bq"
 	"github.com/prnvbn/bq/internal/tfl"
 	"github.com/prnvbn/bq/internal/ui"
 	"github.com/spf13/cobra"
-
-	tea "github.com/charmbracelet/bubbletea"
 )
 
 var (
@@ -29,13 +26,30 @@ var addCmd = &cobra.Command{
 				"🚌 Enter Bus Route",
 				"for e.g. 135 or D7",
 			).Run()
+
+			if errors.Is(err, ui.ErrCancelled) {
+				fmt.Println("User cancelled route input")
+				return
+			}
 			fatal(err, "error running route input")
 		}
 
 		stopPoints, err := c.StopPoints(route)
 		fatal(err, "error getting stop points")
 
-		stopPoint := runStopSelection(stopPoints)
+		stopItemsIter := func(yield func(stopItem) bool) {
+			for stopPoint := range stopPoints {
+				if !yield(stopItem{name: stopPoint.CommonName, letter: stopPoint.Letter, id: stopPoint.ID}) {
+					return
+				}
+			}
+		}
+		stopPoint, err := ui.NewListInputModal(stopItemsIter).Run()
+		if errors.Is(err, ui.ErrCancelled) {
+			fmt.Println("User cancelled stop selection")
+			return
+		}
+		fatal(err, "error running stop selection")
 
 		cfg.Arrivals = append(cfg.Arrivals, bq.Arrival{
 			Route:     route,
@@ -61,24 +75,3 @@ type stopItem struct {
 func (s stopItem) Title() string       { return s.name }
 func (s stopItem) Description() string { return "Stop " + s.letter }
 func (s stopItem) FilterValue() string { return s.name }
-
-func runStopSelection(stopPoints iter.Seq[tfl.StopPoint]) stopItem {
-	stopItemsIter := func(yield func(stopItem) bool) {
-		for stopPoint := range stopPoints {
-			if !yield(stopItem{name: stopPoint.CommonName, letter: stopPoint.Letter, id: stopPoint.ID}) {
-				return
-			}
-		}
-	}
-
-	m := ui.NewListInputModal(stopItemsIter)
-	p := tea.NewProgram(m)
-	finalModel, err := p.Run()
-	if err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
-
-	sm := finalModel.(ui.ListInputModel[stopItem])
-	return sm.Choice()
-}
